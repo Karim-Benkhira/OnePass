7114a83a4873ea38cf8451cf3779d5387bb4c980
Karim Benkhira <karimbenkhira.contact@gmail.com>
2025-03-25 15:06:20 +0000

    Gestions des Ip

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,20 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DeviceController;
+use App\Http\Controllers\IpManagementController;
 
-// Normal authenticated routes
 Route::middleware('auth:sanctum')->group(function () {
-    // Device management routes
+
     Route::post('/devices/check', [DeviceController::class, 'checkDevice']);
     Route::get('/devices', [DeviceController::class, 'listDevices']);
     Route::get('/devices/verify', [DeviceController::class, 'verifyDevice']);
+
+
+    Route::prefix('ip')->group(function () {
+        Route::get('/', [IpManagementController::class, 'index']);
+        Route::post('/whitelist', [IpManagementController::class, 'addToWhitelist']);
+        Route::post('/blacklist', [IpManagementController::class, 'addToBlacklist']);
+        Route::delete('/remove', [IpManagementController::class, 'remove']);
+        Route::get('/check/{ip}', [IpManagementController::class, 'checkIp']);
+    });
 });
